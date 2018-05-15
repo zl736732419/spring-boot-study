95d140e4ca78d469fc4d5b7243e858fb3894ec03
zhenglian <736732419@qq.com>
2018-05-15 18:30:14 +0800

    自定义类型转换，未完待续。。。

--- src/main/java/com/zheng/springboot/configuration/validate/CustomDateConversionService.java
+++ src/main/java/com/zheng/springboot/configuration/validate/CustomDateConversionService.java
@@ -4,30 +4,47 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.lang.Nullable;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @Author zhenglian
  * @Date 2018/5/15 18:13
  */
-public class CustomConversionService implements ConversionService {
+public class CustomDateConversionService implements ConversionService {
+    private String pattern = "yyyy-MM-dd HH:mm:ss";
+    
     @Override
     public boolean canConvert(@Nullable Class<?> sourceType, Class<?> targetType) {
+        if (sourceType == String.class && targetType == Date.class) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean canConvert(@Nullable TypeDescriptor sourceType, TypeDescriptor targetType) {
-        return false;
+        return canConvert(sourceType.getType(), targetType.getType());
     }
 
     @Nullable
     @Override
     public <T> T convert(@Nullable Object source, Class<T> targetType) {
-        return null;
+        String time = (String) source;
+        SimpleDateFormat sf = new SimpleDateFormat(pattern);
+        T date = null;
+        try {
+            date = (T) sf.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
     }
 
     @Nullable
     @Override
     public Object convert(@Nullable Object source, @Nullable TypeDescriptor sourceType, TypeDescriptor targetType) {
-        return null;
+        return convert(source, targetType.getType());
     }
 }
--- src/main/java/com/zheng/springboot/configuration/validate/ValidateConfiguration.java
+++ src/main/java/com/zheng/springboot/configuration/validate/ValidateConfiguration.java
@@ -1,15 +1,19 @@
 package com.zheng.springboot.configuration.validate;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Validator;
 
 /**
  * @Author zhenglian
  * @Date 2018/5/15 18:02
  */
-//@Component
+@Component
 public class ValidateConfiguration {
-//    @Bean
+    @Bean
     public static Validator configurationPropertiesValidator() {
         return new CustomServerValidator();
     }
+    
+    
 }
