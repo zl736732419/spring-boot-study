57c01bc057000a044e74cef30340de05083ace47
zhenglian <736732419@qq.com>
2018-05-16 16:38:26 +0800

    添加thymeleaf,freemarker模板

--- src/main/java/com/zheng/springboot/helloworld/HelloWorldApp.java
+++ src/main/java/com/zheng/springboot/helloworld/HelloWorldApp.java
@@ -2,7 +2,7 @@ package com.zheng.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class HelloWorldApp {
     
-    @RequestMapping("/hello")
+    @GetMapping(value = "/hello")
     public String home() {
         return "hello world";
     }
