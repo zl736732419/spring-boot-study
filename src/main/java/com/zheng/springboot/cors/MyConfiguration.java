baf6d7ef1efd40589a82aa2ce1f41c5de6ecae1e
z736732419 <736732419@qq.com>
2018-05-16 23:51:57 +0800

    添加cors跨域，待测试

---
+++ src/main/java/com/zheng/springboot/cors/MyConfiguration.java
@@ -0,0 +1,23 @@
+package com.zheng.springboot.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 设置cors跨域访问
+ */
+@Configuration
+public class MyConfiguration {
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**");
+			}
+		};
+	}
+}
