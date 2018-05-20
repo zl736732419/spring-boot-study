892c373366f9aa26c585643523a69cbc869d35fe
z736732419 <736732419@qq.com>
2018-05-20 11:57:40 +0800

    添加请求id标识

--- src/main/java/com/zheng/springboot/message/rabbitmq/RabbitmqSender.java
+++ src/main/java/com/zheng/springboot/message/rabbitmq/RabbitmqSender.java
@@ -3,8 +3,11 @@ package com.zheng.springboot.message.rabbitmq;
 import com.zheng.springboot.mybatis.domain.User;
 import com.zheng.springboot.mybatis.service.UserService;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * @Author zhenglian
@@ -19,23 +22,27 @@ public class RabbitmqSender {
     
     public void hello() {
         String context = "hello rabbit";
-        rabbitTemplate.convertAndSend("hello", context);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend("hello", (Object) context, correlationData);
     }
 
     public void user(Integer userId) {
         User user = userService.findById(userId);
-        rabbitTemplate.convertAndSend("hello", user);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
+        rabbitTemplate.convertAndSend("hello", user, correlationData);
     }
 
     public void topic(Integer userId) {
         User user = userService.findById(userId);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
         rabbitTemplate.convertAndSend(RabbitmqConfig.TOPIC_EXCHANGE, 
-                "topic.helloworld", user);
+                "topic.helloworld", user, correlationData);
     }
 
     public void fanoutMessage(Integer userId) {
         User user = userService.findById(userId);
+        CorrelationData correlationData = new CorrelationData(UUID.randomUUID().toString());
         rabbitTemplate.convertAndSend(RabbitmqConfig.FANOUT_EXCHANGE,
-                "", user);
+                "", user, correlationData);
     }
 }
